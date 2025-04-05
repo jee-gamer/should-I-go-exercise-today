76cceb469bf09ed3f9dfe1ca43b0e2f2652a5fd0
Jiramate Kedmake <jiramate2548@gmail.com>
2025-04-05 14:51:26 +0700

    Fix page color and add a font for the topic

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import '@/app/globals.css'
+import localFont from 'next/font/local'
+
+const CaveatBrush_font = localFont({src: './fonts/CaveatBrush-Regular.ttf'})
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-amber-50">
-      <div className="flex flex-col items-center justify-center gap-16 mt-40 text-5xl text-black"> Should I go exercise today?
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] notebook">
+      <div className={`flex flex-col items-center justify-center gap-16 mt-100 text-8xl text-black ${CaveatBrush_font.className}`}> Should I go exercise right now?
       </div>
 
         <div id="main" className="flex flex-row items-center justify-center gap-16 mt-20">
