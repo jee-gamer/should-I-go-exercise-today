8524c9bbc975243757eca8383f53ff5e259dc3aa
Jiramate Kedmake <jiramate2548@gmail.com>
2025-04-05 14:22:06 +0700

    Add lines to the page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import '@/app/globals.css'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-amber-50">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] notebook  bg-amber-50">
       <div className="flex flex-col items-center justify-center gap-16 mt-40 text-5xl text-black"> Should I go exercise today?
       </div>
 
