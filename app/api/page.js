dce131b3c32b51b5fdfd9b6af822c105c38a8bce
Jiramate Kedmake <jiramate2548@gmail.com>
2025-04-11 11:37:31 +0700

    Remove border from API page

--- app/api/page.js
+++ app/api/page.js
@@ -5,10 +5,10 @@ import { CaveatBrush_font, PM_font, WinkySans_font } from "@/app/Fonts";
 export default function Api() {
     return (
       <div className="flex flex-col items-center justify-items-center min-h-fit sm:p-20 font-[family-name:var(--font-geist-sans)] notebook">
-        <div id="main" className="flex flex-col items-center justify-center min-w-full border">
+        <div id="main" className="flex flex-col items-center justify-center min-w-full">
             <span className={`text-9xl ${CaveatBrush_font.className} mt-10 text-outline`}> APIs </span>
             <span className={`inline-block text-left text-4xl ${WinkySans_font.className} mt-20 w-5/6`}> Base URL: http://localhost:3000/api </span>
-            <div id="apiBoxes" className="flex flex-col items-start justify-center w-5/6 gap-16 mt-20 border">
+            <div id="apiBoxes" className="flex flex-col items-start justify-center w-5/6 gap-16 mt-20">
                 <div className="flex flex-row justify-center w-full pl-10 pr-10">
                     <span className={`text-4xl w-1/3 ${PM_font.className}`}>API</span>
                     <span className={`text-4xl w-1/3 ${PM_font.className}`} >Path</span>
