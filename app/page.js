27c2c3fb260d98a14a20a4b8a2dbe50c861e5b41
Jiramate Kedmake <jiramate2548@gmail.com>
2025-04-11 10:51:38 +0700

    Add time buttons

--- app/page.js
+++ app/page.js
@@ -12,12 +12,18 @@ export default function Home() {
 	const [temp, setTemp] = useState(31); // temp here
 	const [tempColor, setTempColor] = useState("");
 
+	const [time, setTime] = useState("now");
+
 	useEffect(() => {
 		if (temp <= 26) setTempColor("text-blue-500");
 		else if (temp <= 30) setTempColor("text-yellow-500");
 		else if (temp <= 35) setTempColor("text-orange-500");
 		else setTempColor("text-red-800");
 	}, [temp])
+
+	useEffect(() => {
+		// To change to prediction when set new time
+	}, [time])
 
   return (
     <div className="relative flex flex-col items-center justify-items	-center min-h-fit w-full max-w-screen sm:p-20 font-[family-name:var(--font-geist-sans)] notebook overflow-x-hidden z-0">
@@ -33,22 +39,23 @@ export default function Home() {
 				width={ 700 }
 				height={ 700 }
 				alt="Wood Sign"
-				className="absolute transform -translate-x-220 translate-y-90 -rotate-80"
+				className="absolute transform -translate-x-220 translate-y-70 -rotate-80"
 			/>
 			<Image
 				src="/eraser1.png"
 				width={ 300 }
 				height={ 300 }
 				alt="Wood Sign"
-				className="absolute transform -translate-x-210 translate-y-220"
+				className="absolute transform -translate-x-210 translate-y-200"
 			/>
-      <div className={`flex flex-col items-center justify-center gap-16 mt-40 text-8xl text-black text-outline ${CaveatBrush_font.className}`}> Should I go exercise right now?
-      </div>
+			<span className={`flex flex-col items-center justify-center gap-16 mt-20 text-8xl text-black text-outline ${CaveatBrush_font.className}`}>Should I go exercise today?</span>
+			<span className={`text-4xl ${WinkySans_font.className} text-gray-500 mt-10 `}>time - {time}</span>
+
 
 			<div id="main" className="flex flex-row items-center justify-center gap-16 min-w-[1304px]">
 
 				<div id="recommendation"
-						 className="flex flex-col items-center justify-items-start gap-16 m-30 min-w-[400px] min-h-[500px] max-h-[500px] text-black">
+						 className="flex flex-col items-center justify-items-start gap-16 m-30 mt-10 min-w-[400px] min-h-[500px] max-h-[500px] text-black">
 					<div id="yesno"
 							 className={ `flex flex-col gap-16 text-9xl mt-10 text-outline ${ PM_font.className }` }>
 						YES
@@ -82,9 +89,97 @@ export default function Home() {
 
 			</div>
 
+			{/*TIME BUTTONS*/}
+			<div id="time" className="flex flex-col items-center justify-items-start min-w-full h-4/5 mt-20">
+				<span className={`text-5xl ${CaveatBrush_font.className}`}>Don&#39;t want to exercise now? Tell us when</span>
+				<div id="buttons" className="flex flex-row items-center justify-center min-w-full h-full gap-10 mt-10">
+					<div name="button" className="flex-width-time h-full">
+						<button className={`time-button bg-gradient-to-r from-blue-900 to-blue-200`}
+						onClick={() => setTime("Dawn")}>
+							<span className={`${CaveatBrush_font.className} text-blue-900`}>Dawn</span>
+						</button>
+						<span className={`text-4xl ${WinkySans_font.className} text-gray-500`}>7-9 PM</span>
+					</div>
+					<div name="button" className="flex-width-time h-full">
+						<button className={`time-button bg-gradient-to-r from-blue-200 to-yellow-200`}
+										onClick={() => setTime("Morning")}>
+							<span className={`${CaveatBrush_font.className} text-blue-900`}>Morning</span>
+						</button>
+						<span className={`text-4xl ${WinkySans_font.className} text-gray-500`}>9-11 PM</span>
+					</div>
+					<div name="button" className="flex-width-time h-full">
+						<button className={`time-button bg-gradient-to-r from-yellow-200 to-yellow-500`}
+										onClick={() => setTime("Noon")}>
+							<span className={`${CaveatBrush_font.className} text-blue-900`}>Noon</span>
+						</button>
+						<span className={`text-4xl ${WinkySans_font.className} text-gray-500`}>11-13 PM</span>
+					</div>
+					<div name="button" className="flex-width-time h-full">
+						<button className={`time-button bg-gradient-to-r from-yellow-500 to-orange-500`}
+										onClick={() => setTime("Afternoon")}>
+							<span className={`${CaveatBrush_font.className} text-blue-900`}>Afternoon</span>
+						</button>
+						<span className={`text-4xl ${WinkySans_font.className} text-gray-500`}>13-15 PM</span>
+					</div>
+					<div name="button" className="flex-width-time h-full">
+						<button className={`time-button bg-gradient-to-r from-orange-500 to-blue-200`}
+										onClick={() => setTime("Late Afternoon")}>
+							<span className={`${CaveatBrush_font.className} text-blue-900`}>Late Afternoon</span>
+						</button>
+						<span className={`text-4xl ${WinkySans_font.className} text-gray-500`}>15-17 PM</span>
+					</div>
+				</div>
+			</div>
+
+			{/*ATTRIBUTE BOXES*/}
+			<div id="attributes" className="flex flex-row items-start justify-center min-w-full min-h-1/4 gap-[5vw] mt-50 mb-30">
+
+				<div className="flex-width">
+					<div className="attribute-box">
+						<span className={`${CaveatBrush_font.className} attribute-title`}>🌡️ Temperature</span>
+						<div className="flex flex-row items-end">
+							<span id="temperature" className={`${ WinkySans_font.className } ${tempColor} attribute-text`}>{ temp }</span>
+							<span className={`${ WinkySans_font.className } ${tempColor} text-2xl xl:text-5xl mb-4`}>°C</span>
+						</div>
+					</div>
+					<span className={`${ WinkySans_font.className } text-4xl mt-5`}>High Risk of heat stroke</span>
+				</div>
+
+				<div className="flex-width">
+					<div className="attribute-box">
+						<span className={`${CaveatBrush_font.className} attribute-title`}>💦 Humidity</span>
+						<div className="flex flex-row items-end text-blue-500">
+							<span id="humidity" className={`${ WinkySans_font.className } attribute-text`}>80</span>
+							<span className={`${ WinkySans_font.className } text-2xl xl:text-6xl mb-4`}>%</span>
+						</div>
+					</div>
+					<span className={`${ WinkySans_font.className } text-4xl mt-5`}>High Risk of heat stroke</span>
+				</div>
+
+				<div className="flex-width">
+					<div className="attribute-box">
+						<span className={`${CaveatBrush_font.className} attribute-title`}>😶‍🌫 PM 2.5 (μg/m3)</span>
+						<span id="pm25" className={`${ WinkySans_font.className } attribute-text text-gray-500`}>145</span>
+					</div>
+					<span className={`${ WinkySans_font.className } text-4xl mt-5`}>High Risk of heat stroke</span>
+				</div>
+
+				<div className="flex-width">
+					<div id="people" className="attribute-box">
+						<span className={`${CaveatBrush_font.className} attribute-title`}>👨‍👨 People</span>
+						<div className="flex flex-row items-end">
+							<span id="people" className={`${ WinkySans_font.className } attribute-text`}>64</span>
+							<span className={`${ WinkySans_font.className } text-2xl xl:text-6xl mb-4`}>%</span>
+						</div>
+					</div>
+					<span className={`${ WinkySans_font.className } text-4xl mt-5`}>Many people might go</span>
+				</div>
+
+			</div>
+
 			<Link
 				href="/api"
-				className={`relative flex flex-col items-center justify-center min-w-100 max-w-200 gap-16 mt-10 text-5xl text-black rounded-2xl p-2 hover:text-gray-600 hover:opacity-70 ${ PM_font.className }`}
+				className={`relative flex flex-col items-center justify-center min-w-100 max-w-200 gap-16 mt-10 mb-40 text-5xl text-black rounded-2xl p-2 hover:text-gray-600 hover:opacity-70 ${ PM_font.className }`}
 			>
 				Go to API
 				<Image
@@ -95,52 +190,6 @@ export default function Home() {
 					className="absolute -top-15 -left-5 min-w-100"
 				/>
 			</Link>
-
-
-
-			<div id="attributes" className="flex flex-row items-start justify-center min-w-full min-h-1/4 gap-[5vw] mt-40 mb-30 border">
-				<div className="normal-flex">
-					<div className="attribute-box">
-						<span className={`${CaveatBrush_font.className} attribute-title border`}>🌡️ Temperature</span>
-						<div className="flex flex-row items-end">
-							<span id="temperature" className={`${ WinkySans_font.className } ${tempColor} attribute-text`}>{ temp }</span>
-							<span className={`${ WinkySans_font.className } ${tempColor} text-2xl xl:text-5xl mb-4`}>°C</span>
-						</div>
-					</div>
-					<span className={`${ WinkySans_font.className } text-4xl mt-5`}>High Risk of heat stroke</span>
-				</div>
-
-				<div className="normal-flex">
-					<div className="attribute-box">
-						<span className={`${CaveatBrush_font.className} attribute-title border`}>💦 Humidity</span>
-						<div className="flex flex-row items-end text-blue-500">
-							<span id="humidity" className={`${ WinkySans_font.className } attribute-text`}>80</span>
-							<span className={`${ WinkySans_font.className } text-2xl xl:text-6xl mb-4`}>%</span>
-						</div>
-					</div>
-					<span className={`${ WinkySans_font.className } text-4xl mt-5`}>High Risk of heat stroke</span>
-				</div>
-
-				<div className="normal-flex">
-					<div className="attribute-box">
-						<span className={`${CaveatBrush_font.className} attribute-title border`}>😶‍🌫 PM 2.5 (μg/m3)</span>
-						<span id="pm25" className={`${ WinkySans_font.className } attribute-text text-gray-500`}>145</span>
-					</div>
-					<span className={`${ WinkySans_font.className } text-4xl mt-5`}>High Risk of heat stroke</span>
-				</div>
-
-				<div className="normal-flex">
-					<div id="people" className="attribute-box">
-						<span className={`${CaveatBrush_font.className} attribute-title border`}>👨‍👨 People</span>
-						<div className="flex flex-row items-end">
-							<span id="people" className={`${ WinkySans_font.className } attribute-text`}>64</span>
-							<span className={`${ WinkySans_font.className } text-2xl xl:text-6xl mb-4`}>%</span>
-						</div>
-					</div>
-					<span className={`${ WinkySans_font.className } text-4xl mt-5`}>Many people might go</span>
-				</div>
-
-			</div>
 
 		</div>
   );
