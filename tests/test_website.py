5664f8153638560ee5688da25513274d823d9a75
Jiramate Kedmake <jiramate2548@gmail.com>
2025-04-13 11:15:16 +0700

    Fix unittests (Run by python -m unittest test_website.py) You must be in tests cd first

--- tests/test_website.py
+++ tests/test_website.py
@@ -3,13 +3,15 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+import time
 
 
 class Test(unittest.TestCase):
 
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.baseURL = "http://localhost:3000/"
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.baseURL = "http://localhost:3000/"
 
     def test_home_page(self):
         """
@@ -21,13 +23,15 @@ class Test(unittest.TestCase):
 
         self.assertIn(suggestion, ["yes", "no", "maybe"])
 
-        time = driver.find_element(by=By.ID, value='currentTime').text.lower()
-        self.assertEqual(time, "time - now")
+        current_time = driver.find_element(by=By.ID, value='currentTime').text.lower()
+        self.assertEqual(current_time, "time - now")
 
         description = driver.find_element(by=By.ID,
                                           value='general').text.lower()
 
         self.assertTrue(len(description) > 10)
+
+        time.sleep(10)
 
     def test_home_page_with_time(self):
         """
@@ -38,17 +42,19 @@ class Test(unittest.TestCase):
 
         temperature = driver.find_element(by=By.ID, value="temperature").text
         people = driver.find_element(by=By.ID, value="people").text
-        time = driver.find_element(by=By.ID, value='currentTime').text.lower()
+        current_time = driver.find_element(by=By.ID, value='currentTime').text.lower()
 
         buttons = driver.find_elements(by=By.NAME, value="button")  # will get dawn button because it's the first button
         dawn_button = buttons[3]
         dawn_button.click()
 
+        time.sleep(15)
+
         new_temperature = driver.find_element(by=By.ID, value="temperature").text
         new_people = driver.find_element(by=By.ID, value="people").text
         new_time = "time - afternoon"
 
-        self.assertNotEqual(time, new_time)
+        self.assertNotEqual(current_time, new_time)
         self.assertNotEqual(temperature, new_temperature)
         self.assertNotEqual(people, new_people)
 
@@ -69,3 +75,4 @@ class Test(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
