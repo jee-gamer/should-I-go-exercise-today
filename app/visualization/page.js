27b05696defc89692596f09334a959625b72b593
Jiramate Kedmake <jiramate2548@gmail.com>
2025-04-15 11:40:46 +0700

    Refactor Chart lib and add more charts

--- app/visualization/page.js
+++ app/visualization/page.js
@@ -1,7 +1,7 @@
 'use client';
 
-import {useEffect, useRef, useState} from 'react';
-import createChart from "@/lib/Graph";
+import {useEffect, useState} from 'react';
+import {BarChart, LineChart} from "@/lib/CanvasMaker";
 
 import '@/app/globals.css'
 
@@ -19,104 +19,51 @@ export default function Visualization() {
 		console.log(`Changed weather attribute to ${weather}`)
 	}, [weather])
 
-	const tempPeople = useRef(null);
-	const humidPeople = useRef(null);
-	const rainPeople = useRef(null);
-	const pm25People = useRef(null);
-
-	useEffect(() => {
-		const ctx = tempPeople.current?.getContext('2d');
-
-		if (!ctx) return;
-
-		const X = ['25', '31', '32', '33', '40'];
-		const xLabel = "Temperature (°C)"
-		const Y = [12, 19, 3, 5, 8];
-		const yLabel = "People"
-
-		const chart = createChart(ctx,X, Y, xLabel, yLabel);
-
-		return () => chart.destroy(); // cleanup on unmount
-	}, []);
-
-	useEffect(() => {
-		const ctx = humidPeople.current?.getContext('2d');
-
-		if (!ctx) return;
-
-		const X = ['78', '50', '90', '81', '76'];
-		const xLabel = "Humidity (%)"
-		const Y = [12, 19, 3, 5, 8];
-		const yLabel = "People"
-
-		const chart = createChart(ctx,X, Y, xLabel, yLabel);
-
-		return () => chart.destroy(); // cleanup on unmount
-	}, []);
-
-	useEffect(() => {
-		const ctx = rainPeople.current?.getContext('2d');
-
-		if (!ctx) return;
-
-		const X = ['78', '50', '90', '81', '76'];
-		const xLabel = "Precip (mm)"
-		const Y = [12, 19, 3, 5, 8];
-		const yLabel = "People"
-
-		const chart = createChart(ctx,X, Y, xLabel, yLabel);
-
-		return () => chart.destroy(); // cleanup on unmount
-	}, []);
-
-	useEffect(() => {
-		const ctx = pm25People.current?.getContext('2d');
-
-		if (!ctx) return;
-
-		const X = ['78', '50', '90', '81', '76'];
-		const xLabel = "PM 2.5 (μg/m3)"
-		const Y = [12, 19, 3, 5, 8];
-		const yLabel = "People"
-
-		const chart = createChart(ctx,X, Y, xLabel, yLabel);
-
-		return () => chart.destroy(); // cleanup on unmount
-	}, []);
-
-
-
 	return (
 		<div className="flex flex-col items-center justify-items-center min-h-fit h-screen sm:p-20 font-[family-name:var(--font-geist-sans)] text-black notebook-wo-line">
 			<div id="main" className="flex flex-col items-center justify-items-start min-w-full min-h-screen border">
 				<span className={`text-9xl ${CaveatBrush_font.className} mt-10 text-outline`}> Visualization </span>
-				<span className={`inline-block text-left text-4xl ${WinkySans_font.className} mt-20 w-5/6`}> Relationship Graph between people and weather</span>
-
-				<div className="flex flex-row items-center justify-center w-5/6 gap-16 mt-20">
-					<span className={`flex justify-center w-1/2 text-3xl mb-5 ${WinkySans_font.className}`}> Temperature</span>
-					<span className={`flex justify-center w-1/2 text-3xl mb-5 ${WinkySans_font.className}`}> Humidity</span>
-				</div>
+				<span className={`inline-block text-left text-4xl ${WinkySans_font.className} mt-20 w-5/6 mb-15`}> Relationship Graph between people and weather</span>
 
 				<div id="graph1" className="flex flex-row items-start justify-center w-5/6 h-[30vh] gap-16 border">
 					<div className="w-1/2 h-full">
-						<canvas className="bg-white" ref={tempPeople}></canvas>
+						<LineChart
+							xData={['25', '31', '32', '33', '40']}
+							yData={[12, 19, 3, 5, 8]}
+							xLabel="Temperature (°C)"
+							yLabel="People"
+							title="Temperature"
+						/>
 					</div>
 					<div className="w-1/2 h-full">
-						<canvas className="bg-white" ref={humidPeople}></canvas>
+						<LineChart
+							xData={['25', '31', '32', '33', '40']}
+							yData={[12, 19, 3, 5, 8]}
+							xLabel="Temperature (°C)"
+							yLabel="People"
+							title="Humidity"
+						/>
 					</div>
 				</div>
 
-				<div className="flex flex-row items-center justify-center w-5/6 gap-16 mt-20">
-					<span className={`flex justify-center w-1/2 text-3xl mb-5 ${WinkySans_font.className}`}> Rain</span>
-					<span className={`flex justify-center w-1/2 text-3xl mb-5 ${WinkySans_font.className}`}> PM 2.5</span>
-				</div>
-
-				<div id="graph2" className="flex flex-row items-start justify-center w-5/6 h-[30vh] gap-16 border">
+				<div id="graph2" className="flex flex-row items-start justify-center w-5/6 h-[30vh] gap-16 mt-10 border">
 					<div className="w-1/2 h-full">
-						<canvas className="bg-white" ref={rainPeople}></canvas>
+						<LineChart
+							xData={['25', '31', '32', '33', '40']}
+							yData={[12, 19, 3, 5, 8]}
+							xLabel="Temperature (°C)"
+							yLabel="People"
+							title="Precip_mm"
+						/>
 					</div>
 					<div className="w-1/2 h-full">
-						<canvas className="bg-white" ref={pm25People}></canvas>
+						<LineChart
+							xData={['25', '31', '32', '33', '40']}
+							yData={[12, 19, 3, 5, 8]}
+							xLabel="Temperature (°C)"
+							yLabel="People"
+							title="PM 2.5"
+						/>
 					</div>
 				</div>
 
@@ -148,7 +95,28 @@ export default function Visualization() {
 				{/*	Put your graph here Putter, How you get the data from the Time and Weather value is up to you */}
 				</div>
 
-				<span className={`inline-block text-left text-4xl ${WinkySans_font.className} mt-20 w-5/6`}> More fucking graph..</span>
+				<span className={`inline-block text-left text-4xl ${WinkySans_font.className} mt-20 w-5/6`}> Bar Chart comparing amount of people throughout the day</span>
+				<div id="graph2" className="flex flex-row items-start justify-center w-5/6 h-[30vh] gap-16 border mt-15">
+					<div className="flex flex-col items-center w-1/2 h-full">
+						<BarChart
+							xData={['25', '31', '32', '33', '40']}
+							yData={[12, 19, 3, 5, 8]}
+							xLabel="Temperature (°C)"
+							yLabel="People"
+							title="Average people"
+						/>
+					</div>
+					<div className="flex flex-col items-center w-1/2 h-full">
+						<BarChart
+							xData={['25', '31', '32', '33', '40']}
+							yData={[12, 19, 3, 5, 8]}
+							xLabel="Temperature (°C)"
+							yLabel="People"
+							title="Average people"
+						/>
+					</div>
+				</div>
+
 
 			</div>
 		</div>
