f9d03f49d62b199d3d3c53557355fe3915f62510
0CreepySmile0 <yutivich2005@gmail.com>
2025-04-17 19:18:29 +0700

    Add docstring for APIs

--- app/api/suggestion/route.js
+++ app/api/suggestion/route.js
@@ -1,6 +1,15 @@
 import recommendation from "@/lib/Recommendation";
 
 export async function GET(req) {
+  `
+    Get suggestion base on weather condition provided by WeatherAPI from given time
+  possible time param: ['dawn', 'morning', 'noon', 'afternoon', 'late-afternoon']
+  if omitted, it will use current time and if not fall in any possible time param
+  the result will be unavailable.
+  example use: baseurl/api/suggestion; returns suggestion and description of current time.
+  example use: baseurl/api/suggestion?time=dawn; returns suggestion and description at 8 AM of today
+  return: { suggestion: string, description: string }
+  `
   // possible time param: ['dawn', 'morning', 'noon', 'afternoon', 'late-afternoon']
   // example: /api/suggestion?time=dawn: this will response suggestion at 8 AM of today
   const searchParam = new URLSearchParams(new URL(req.url).search);
