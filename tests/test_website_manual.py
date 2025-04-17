8daba789f46b2b5035764b59f52071e710384f9c
Jiramate Kedmake <jiramate2548@gmail.com>
2025-04-17 19:35:27 +0700

    Fix some unit tests and add pytest to requirements

--- tests/test_website_manual.py
+++ tests/test_website_manual.py
@@ -1,24 +1,26 @@
 import unittest
 import requests
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
 
-class Test(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.driver = webdriver.Chrome()
-        cls.baseURL = "http://localhost:3000/"
+class SeleniumTest(unittest.TestCase):
 
-    def test_home_page(self):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.baseURL = "http://localhost:3000"
+        self.driver.get(self.baseURL)
+        self.actions = ActionChains(self.driver)
+
+    def test_01_home_page(self):
         """
         Test that the home page have to show the suggestion based on the current time
         """
         driver = self.driver
-        driver.get(self.baseURL)
         suggestion = driver.find_element(by=By.ID, value='yesno').text.lower()
 
         self.assertIn(suggestion, ["yes", "no", "maybe"])
@@ -33,46 +35,59 @@ class Test(unittest.TestCase):
 
         time.sleep(7)
 
-    def test_home_page_with_time(self):
+    def test_02_home_page_with_time(self):
         """
         Test that the home page have to show the suggestion after pressing time button
         """
         driver = self.driver
-        driver.get(self.baseURL)
 
         temperature = driver.find_element(by=By.ID, value="temperature").text
         people = driver.find_element(by=By.ID, value="people").text
         current_time = driver.find_element(by=By.ID, value='currentTime').text.lower()
 
         buttons = driver.find_elements(by=By.NAME, value="button")  # will get dawn button because it's the first button
-        dawn_button = buttons[3]
+        dawn_button = buttons[0]
 
         time.sleep(4)
 
-        dawn_button.click()
+        self.actions.click_and_hold(dawn_button).perform()
+        # Hold the click for 2 seconds
+        time.sleep(2)
+        # Release the click
+        self.actions.release().perform()
 
+        time.sleep(2)
         new_temperature = driver.find_element(by=By.ID, value="temperature").text
         new_people = driver.find_element(by=By.ID, value="people").text
-        new_time = "time - afternoon"
+        new_time = "time - dawn"
 
         self.assertNotEqual(current_time, new_time)
         self.assertNotEqual(temperature, new_temperature)
         self.assertNotEqual(people, new_people)
 
-    def test_home_page_api_button(self):
+        time.sleep(5)
+
+    def test_03_home_page_api_button(self):
         driver = self.driver
-        driver.get(self.baseURL)
 
         api_button = driver.find_element(by=By.ID, value="api")
 
         time.sleep(5)
+        self.actions.click_and_hold(api_button).perform()
 
-        api_button.click()
+        time.sleep(2)
+        self.actions.release().perform()
 
+        time.sleep(2)
         current_url = driver.current_url
         self.assertEqual(f"{self.baseURL}/api", current_url)
 
         time.sleep(3)
+
+
+class APITest(unittest.TestCase):
+    def setUp(self):
+        self.baseURL = "http://localhost:3000/"
 
     def test_api(self):
         """
@@ -80,27 +95,28 @@ class Test(unittest.TestCase):
         You will need to input the location or use your location
         """
 
-        response = requests.get(f"{self.baseURL}api/suggestion")
+        response = requests.get(f"{self.baseURL}/api/suggestion")
+        data = response.json()
         self.assertEqual(response.status_code, 200)
-        self.assertIn(response.text, ["yes", "no", "maybe"])
+        self.assertIn(data['suggestion'], ["yes", "no", "maybe"])
 
-        response = requests.get(f"{self.baseURL}api/people-at")
-        self.assertEqual(response.status_code, 200)
-        self.assertIsInstance(int(response.text), int)
-
-        response = requests.get(f"{self.baseURL}api/people-now")
+        response = requests.get(f"{self.baseURL}/api/people-at")
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(int(response.text), int)
 
-        response = requests.get(f"{self.baseURL}api/average-people")
+        response = requests.get(f"{self.baseURL}/api/people-now")
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(int(response.text), int)
+
+        response = requests.get(f"{self.baseURL}/api/average-people")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, "6")  # rounded down from 6.5634
 
-        response = requests.get(f"{self.baseURL}api/max-people")
+        response = requests.get(f"{self.baseURL}/api/max-people")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, "39")
 
-        response = requests.get(f"{self.baseURL}api/min-people")
+        response = requests.get(f"{self.baseURL}/api/min-people")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, "0")
 
@@ -112,9 +128,11 @@ class Test(unittest.TestCase):
             'lat': 10.3212,
             'lon': 13.1323
         }
-        response = requests.get(f"{self.baseURL}api/suggestion", params=params)
+        response = requests.get(f"{self.baseURL}/api/suggestion", params=params)
+        data = response.json()
         self.assertEqual(response.status_code, 200)
-        self.assertIn(response.text, ["yes", "no", "maybe"])
+        self.assertIn(data['suggestion'].lower(), ["yes", "no", "maybe"])
+        self.assertTrue(len(data['description']) > 20)
 
     def test_max_people(self):
         """
@@ -123,7 +141,7 @@ class Test(unittest.TestCase):
         params = {
             'time': 'Dawn'
         }
-        response = requests.get(f"{self.baseURL}api/max-people", params=params)
+        response = requests.get(f"{self.baseURL}/api/max-people", params=params)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, "5??")
 
