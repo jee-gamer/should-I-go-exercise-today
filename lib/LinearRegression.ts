8599867e2898798f49e2d4ef4fe00fc410509c15
0CreepySmile0 <yutivich2005@gmail.com>
2025-04-21 00:05:14 +0700

    Refactor to remove warning

--- lib/LinearRegression.ts
+++ lib/LinearRegression.ts
@@ -4,31 +4,31 @@ import WeatherAPI from "@/lib/WeatherAPI";
 
 
 class LinearRegression {
-    readonly X: number[][] // a row can have many column
-    readonly Y: number[][]
+    readonly X: number[][]; // a row can have many column
+    readonly Y: number[][];
     readonly maxPeople: number;
-    readonly regressionModel: any
-    private static instance: LinearRegression
+    readonly regressionModel: any;
+    private static instance: LinearRegression;
 
 
      private constructor(X: number[][], Y: number[][], maxPeople: number) {
         this.X = X;
         this.Y = Y;
         this.maxPeople = maxPeople;
-        this.regressionModel = new MultivariateLinearRegression(X, Y)
+        this.regressionModel = new MultivariateLinearRegression(X, Y);
     }
 
     public static async getInstance() {
         if (!LinearRegression.instance) {
             const data = await DBQuery.getFields(["temperature", "humidity", "people"]);
-            if (!data.error_message) {
-                const X = data.result.map(row => row.slice(0, 2))
-                const Y = data.result.map(row => row.slice(2))
+            if (!data["error_message"]) {
+                const X = data["result"].map(row => row.slice(0, 2));
+                const Y = data["result"].map(row => row.slice(2));
                 const maxPeople = (await DBQuery.getAggregate(["people"], "max"))["people"];
                 LinearRegression.instance = new LinearRegression(X, Y, maxPeople);
             }
         }
-        return LinearRegression.instance
+        return LinearRegression.instance;
     }
 
     async predict(time?: string, lat?: string, lon?: string): Promise<{prediction: number, percentage: number}> {
